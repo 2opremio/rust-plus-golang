ce27e62bb3b83ab9f50911e8601484fb33c3147a
Alfonso Acosta <fons@syntacticsugar.consulting>
2022-11-09 03:50:54 +0100

    Add CI workflow testing multiple platforms (#2)

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ package main
 // The -ldl is sometimes necessary to fix linker errors about `dlsym`.
 
 /*
-#cgo darwin linux LDFLAGS: ./lib/preflight/target/release/libpreflight.a -ldl
-#cgo windows LDFLAGS: ./lib/preflight/target/release/libpreflight.a
 #include "./lib/preflight.h"
 #include <stdlib.h>
 */
